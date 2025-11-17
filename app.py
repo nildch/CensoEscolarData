dcb7259275d77fc44684157eb31db1ea3d3e6e2b
Nielen Jhose <96541549+nildch@users.noreply.github.com>
2025-11-17 18:54:53 -0300

    Api REST

--- app.py
+++ app.py
@@ -1,54 +1,133 @@
-import pandas as pd
-import sqlite3
+from flask import Flask, jsonify, request
+from db import get_connection
 
-CSV_PATH = "data/microdados_ed_basica_2024.csv"
-DB_NAME = "censo_escolar.db"
+app = Flask(__name__)
 
-conn = sqlite3.connect(DB_NAME)
-cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS escolas (
-    id_escola INTEGER PRIMARY KEY,
-    nome_escola TEXT,
-    regiao TEXT,
-    uf TEXT,
-    municipio TEXT,
-    dependencia TEXT,
-    etapa_ensino TEXT,
-    qt_matriculas REAL
-)
-""")
-conn.commit()
+# lista as escolas
+@app.get("/escolas")
+def listar_escolas():
+    conn = get_connection()
+    cursor = conn.cursor()
 
-chunk_size = 10000
-chunks = pd.read_csv(CSV_PATH, sep=';', encoding='latin1', chunksize=chunk_size)
+    cursor.execute("SELECT * FROM escolas")
+    dados = cursor.fetchall()
 
-for i, chunk in enumerate(chunks):
-    
-    # escolas paraiba, nordeste.
-    chunk = chunk[(chunk['NO_REGIAO'] == 'Nordeste') & (chunk['NO_UF'] == 'Paraíba')]
+    conn.close()
+    return jsonify([dict(linha) for linha in dados])
 
-    if chunk.empty:
-        continue
 
-    colunas_existentes = [c for c in ['CO_ENTIDADE', 'NO_ENTIDADE', 'NO_REGIAO', 'NO_UF', 'NO_MUNICIPIO', 'TP_DEPENDENCIA', 'TP_ETAPA_ENSINO', 'QT_MAT_BAS'] if c in chunk.columns]
 
-    dados = chunk[colunas_existentes].copy()
+# buscar escola por ID
+@app.get("/escolas/<int:id_escola>")
+def buscar_por_id(id_escola):
+    conn = get_connection()
+    cursor = conn.cursor()
 
-    renomear = {
-        'CO_ENTIDADE': 'id_escola',
-        'NO_ENTIDADE': 'nome_escola',
-        'NO_REGIAO': 'regiao',
-        'NO_UF': 'uf',
-        'NO_MUNICIPIO': 'municipio',
-        'TP_DEPENDENCIA': 'dependencia',
-        'TP_ETAPA_ENSINO': 'etapa_ensino',
-        'QT_MAT_BAS': 'qt_matriculas'
-    }
-    dados.rename(columns=renomear, inplace=True)
-    dados.to_sql('escolas', conn, if_exists='append', index=False)
-    print(f"✅ Chunk {i+1} inserido ({len(dados)} registros da Paraíba).")
+    cursor.execute("SELECT * FROM escolas WHERE id_escola = ?", (id_escola,))
+    dado = cursor.fetchone()
 
-conn.close()
-print("\nMigração concluída!")
+    conn.close()
+
+    if dado:
+        return jsonify(dict(dado))
+    return jsonify({"erro": "Escola não encontrada."}), 404
+
+
+# filtrar por município
+@app.get("/municipios/<nome>")
+def filtrar_por_municipio(nome):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM escolas WHERE municipio LIKE ?",
+        (f"%{nome}%",)
+    )
+    dados = cursor.fetchall()
+
+    conn.close()
+    return jsonify([dict(l) for l in dados])
+
+
+
+# cria 1 nova escola
+@app.post("/escolas")
+def criar_escola():
+    dados = request.get_json()
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO escolas (id_escola, nome_escola, regiao, uf, municipio, dependencia, etapa_ensino, qt_matriculas)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        dados["id_escola"],
+        dados["nome_escola"],
+        dados["regiao"],
+        dados["uf"],
+        dados["municipio"],
+        dados["dependencia"],
+        dados["etapa_ensino"],
+        dados["qt_matriculas"]
+    ))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"mensagem": "Escola adicionada!"}), 201
+
+
+
+# put
+@app.put("/escolas/<int:id_escola>")
+def atualizar(id_escola):
+    dados = request.get_json()
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE escolas SET
+            nome_escola = ?,
+            regiao = ?,
+            uf = ?,
+            municipio = ?,
+            dependencia = ?,
+            etapa_ensino = ?,
+            qt_matriculas = ?
+        WHERE id_escola = ?
+    """, (
+        dados["nome_escola"],
+        dados["regiao"],
+        dados["uf"],
+        dados["municipio"],
+        dados["dependencia"],
+        dados["etapa_ensino"],
+        dados["qt_matriculas"],
+        id_escola
+    ))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"mensagem": "Escola atualizada!"})
+
+
+
+#delete
+@app.delete("/escolas/<int:id_escola>")
+def deletar(id_escola):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM escolas WHERE id_escola = ?", (id_escola,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"mensagem": "Escola removida!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
